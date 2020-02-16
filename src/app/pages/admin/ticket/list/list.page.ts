d7fc643f8417379d871cc22f0ae376fdfbc849cc
pengcl <3559996@qq.com>
2020-02-16 21:32:38 +0800

    0.0.9

--- src/app/pages/admin/ticket/list/list.page.ts
+++ src/app/pages/admin/ticket/list/list.page.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, Inject, OnDestroy} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 
 import {SelectionModel} from '@angular/cdk/collections';
@@ -48,6 +48,7 @@ export class AdminTicketListPage {
     selection = new SelectionModel<any>(true, []);
 
     constructor(private route: ActivatedRoute,
+                @Inject('FILE_PREFIX_URL') public FILE_PREFIX_URL,
                 private authSvc: AuthService,
                 private companySvc: CompanyService,
                 private ticketSvc: TicketService) {
@@ -91,6 +92,7 @@ export class AdminTicketListPage {
     export() {
         this.ticketSvc.export(this.params).subscribe(res => {
             console.log(res);
+            window.location.href = this.FILE_PREFIX_URL + res;
         });
     }
 
