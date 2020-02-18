6bf5960a03762f1d7ec3a0c6e4973be0ed72497b
pengcl <3559996@qq.com>
2020-02-18 20:24:01 +0800

    0.0.9

--- src/app/pages/admin/plan/item/item.component.ts
+++ src/app/pages/admin/plan/item/item.component.ts
@@ -6,6 +6,8 @@ import {PlanService} from '../plan.service';
 import {SelectionModel} from '@angular/cdk/collections';
 import {MatTableDataSource} from '@angular/material';
 
+import {map} from 'rxjs/operators';
+
 @Component({
     selector: 'app-admin-plan-item',
     templateUrl: './item.component.html',
@@ -28,125 +30,127 @@ export class AdminPlanItemPage implements OnInit {
                 private dashboardSvc: DashboardService,
                 private companySvc: CompanyService,
                 private planSvc: PlanService) {
-        planSvc.item(this.id).subscribe(res => {
-            console.log(res);
-            this.data = res;
-            this.dataSource = new MatTableDataSource<any>(res.policys);
-            this.dataSource.data.forEach(row => this.selection.select(row));
-            const pieLabels = ['版权数量', '商标数量', '专利数量'];
-            const pieData = [
-                {
-                    name: '版权数量',
-                    value: res.softwareCopyRight + res.productCopyRight
-                },
-                {
-                    name: '商标数量',
-                    value: res.brandCount
-                },
-                {
-                    name: '专利数量',
-                    value: res.patentCount
-                }
-            ];
-            this.option.pie = {
-                color: ['#3a9cfd', '#3a9aca', '#389868'],
-                legend: {
-                    bottom: 10,
-                    data: pieLabels,
-                    itemWidth: 8,
-                    itemHeight: 8,
-                    textStyle: {
-                        fontSize: 10
-                    }
-                },
-                grid: {
-                    left: '3%',
-                    right: '4%',
-                    bottom: '10%',
-                    containLabel: true
-                },
-                series: [
+        this.route.paramMap.pipe(map(params => this.id = params.get('id'))).subscribe(id => {
+            planSvc.item(this.id).subscribe(res => {
+                this.data = res;
+                this.dataSource = new MatTableDataSource<any>(res.policys);
+                this.dataSource.data.forEach(row => this.selection.select(row));
+                const pieLabels = ['版权数量', '商标数量', '专利数量'];
+                const pieData = [
                     {
-                        name: '访问来源',
-                        type: 'pie',
-                        radius: ['67%', '70%'],
-                        data: [{name: '1', value: '1'}],
-                        hoverOffset: 0,
-                        label: {
-                            show: false
-                        }
+                        name: '版权数量',
+                        value: res.softwareCopyRight + res.productCopyRight
                     },
                     {
-                        name: '访问来源',
-                        type: 'pie',
-                        radius: ['40%', '65%'],
-                        label: {
-                            position: 'inner',
-                            formatter: '{d}%'
-                        },
-                        data: pieData,
-                        itemStyle: {
-                            borderWidth: 2,
-                            borderType: 'solid',
-                            borderColor: '#fff'
-                        },
-                        hoverOffset: 0
+                        name: '商标数量',
+                        value: res.brandCount
                     },
                     {
-                        name: '访问来源',
-                        type: 'pie',
-                        radius: ['35%', '38%'],
-                        data: [{name: '1', value: '1'}],
-                        hoverOffset: 0,
-                        label: {
-                            show: false
-                        }
+                        name: '专利数量',
+                        value: res.patentCount
                     }
-                ]
-            };
-
-            this.option.line = {
-                color: ['#3bcec6'],
-                grid: {
-                    left: '3%',
-                    right: '4%',
-                    bottom: '3%',
-                    containLabel: true
-                },
-                xAxis: {
-                    type: 'value',
-                    boundaryGap: [0, 0.01]
-                },
-                yAxis: {
-                    type: 'category',
-                    data: ['研究生学历以上人数', '企业研发团队人数', '个税申缴人数', '现有员工人数']
-                },
-                series: [
-                    {
-                        type: 'bar',
-                        data: [res.employeeCount, res.taxPayableCount, res.rndCount, res.graduatesCount],
-                        label: {
-                            show: true,
-                            position: 'right',
-                            color: '#21333F'
+                ];
+                this.option.pie = {
+                    color: ['#3a9cfd', '#3a9aca', '#389868'],
+                    legend: {
+                        bottom: 10,
+                        data: pieLabels,
+                        itemWidth: 8,
+                        itemHeight: 8,
+                        textStyle: {
+                            fontSize: 10
+                        }
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '10%',
+                        containLabel: true
+                    },
+                    series: [
+                        {
+                            name: '访问来源',
+                            type: 'pie',
+                            radius: ['67%', '70%'],
+                            data: [{name: '1', value: '1'}],
+                            hoverOffset: 0,
+                            label: {
+                                show: false
+                            }
                         },
-                        itemStyle: {
-                            normal: {
-                                color: (params) => {
-                                    const colorList = ['rgba(13, 215, 141, 1)', 'rgba(254, 174, 77, 1)', '#feae4d'];
-                                    return colorList[params.dataIndex];
-                                }
+                        {
+                            name: '访问来源',
+                            type: 'pie',
+                            radius: ['40%', '65%'],
+                            label: {
+                                position: 'inner',
+                                formatter: '{d}%'
                             },
-                            emphasis: {
-                                shadowBlur: 10,
-                                shadowOffsetX: 0,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            data: pieData,
+                            itemStyle: {
+                                borderWidth: 2,
+                                borderType: 'solid',
+                                borderColor: '#fff'
+                            },
+                            hoverOffset: 0
+                        },
+                        {
+                            name: '访问来源',
+                            type: 'pie',
+                            radius: ['35%', '38%'],
+                            data: [{name: '1', value: '1'}],
+                            hoverOffset: 0,
+                            label: {
+                                show: false
                             }
                         }
-                    }
-                ]
-            };
+                    ]
+                };
+
+                this.option.line = {
+                    color: ['#3bcec6'],
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'value',
+                        boundaryGap: [0, 0.01]
+                    },
+                    yAxis: {
+                        type: 'category',
+                        data: ['研究生学历以上人数', '企业研发团队人数', '个税申缴人数', '现有员工人数']
+                    },
+                    series: [
+                        {
+                            type: 'bar',
+                            data: [res.employeeCount, res.taxPayableCount, res.rndCount, res.graduatesCount],
+                            label: {
+                                show: true,
+                                position: 'right',
+                                color: '#21333F'
+                            },
+                            itemStyle: {
+                                normal: {
+                                    color: (params) => {
+                                        const colorList = ['rgba(13, 215, 141, 1)', 'rgba(254, 174, 77, 1)', '#feae4d'];
+                                        return colorList[params.dataIndex];
+                                    }
+                                },
+                                emphasis: {
+                                    shadowBlur: 10,
+                                    shadowOffsetX: 0,
+                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                                }
+                            }
+                        }
+                    ]
+                };
+            });
         });
+
     }
 
     ngOnInit() {
@@ -163,7 +167,6 @@ export class AdminPlanItemPage implements OnInit {
 
     /** Gets the total cost of all transactions. */
     getTotalCost() {
-        console.log(this.selection.selected);
         let cost = 0;
         this.selection.selected.forEach(item => {
             cost = cost + item.subsidyAmtEnd;
