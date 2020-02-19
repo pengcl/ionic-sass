cbdd97663acdf1729530a0e4aef7d2f84380fee9
pengcl <3559996@qq.com>
2020-02-19 21:32:01 +0800

    0.0.9

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -15,129 +15,129 @@ const routes: Routes = [
             },
             {
                 path: 'dashboard',
-                data: {name: '首页'},
+                data: {name: '首页', menuIndex: null},
                 loadChildren: () =>
                     import('./dashboard/dashboard.module').then(m => m.AdminDashboardPageModule)
             },
             {
                 path: 'order/list',
-                data: {name: '订单'},
+                data: {name: '订单', menuIndex: null},
                 loadChildren: () =>
                     import('./order/list/list.module').then(m => m.AdminOrderListPageModule)
             },
             {
                 path: 'order/item/:id',
-                data: {name: '订单详情'},
+                data: {name: '订单详情', menuIndex: null},
                 loadChildren: () =>
                     import('./order/item/item.module').then(m => m.AdminOrderItemPageModule)
             },
             {
                 path: 'order/checkout/:id',
-                data: {name: '支付订单'},
+                data: {name: '支付订单', menuIndex: null},
                 loadChildren: () =>
                     import('./order/checkout/checkout.module').then(m => m.AdminOrderCheckoutPageModule)
             },
             {// 主体例表
                 path: 'company/list',
-                data: {name: '我的主体'},
+                data: {name: '我的主体', menuIndex: 1},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./company/list/list.module').then(m => m.AdminCompanyListPageModule)
             },
             {// 主体详情
                 path: 'company/item/:id',
-                data: {name: '企业资质'},
+                data: {name: '企业资质', menuIndex: 1},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./company/item/item.module').then(m => m.AdminCompanyItemPageModule)
             },
             {// 生成报告
                 path: 'company/qualification/:id',
-                data: {name: '生成报告'},
+                data: {name: '生成报告', menuIndex: 1},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./company/qualification/qualification.module').then(m => m.AdminCompanyQualificationPageModule)
             },
             {// 生成报告
                 path: 'checkout',
-                data: {name: '自助下单'},
+                data: {name: '自助下单', menuIndex: 5},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./checkout/checkout.module').then(m => m.AdminCheckoutPageModule)
             },
             {// 体检报告列表
                 path: 'plan/list',
-                data: {name: '企业体检报告'},
+                data: {name: '企业体检报告', menuIndex: 6},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./plan/list/list.module').then(m => m.AdminPlanListPageModule)
             },
             {// 体检报告列表
                 path: 'plan/item/:id',
-                data: {name: '企业体检报告'},
+                data: {name: '企业体检报告', menuIndex: 6},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./plan/item/item.module').then(m => m.AdminPlanItemPageModule)
             },
             {// 风险报告列表
                 path: 'risk/list',
-                data: {name: '企业风险报告'},
+                data: {name: '企业风险报告', menuIndex: 6},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./risk/list/list.module').then(m => m.AdminRiskListPageModule)
             },
             {// 体检报告列表
                 path: 'monitor/list',
-                data: {name: '竞争对手监控'},
+                data: {name: '竞争对手监控', menuIndex: null},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./monitor/list/list.module').then(m => m.AdminMonitorListPageModule)
             },
             {// 托管例表
                 path: 'trust/list',
-                data: {name: '知产托管'},
+                data: {name: '知产托管', menuIndex: 2},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./trust/list/list.module').then(m => m.AdminTrustListPageModule)
             },
             {// 托管详情
                 path: 'trust/item/:id',
-                data: {name: '专利托管详情'},
+                data: {name: '专利托管详情', menuIndex: 2},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./trust/item/item.module').then(m => m.AdminTrustItemPageModule)
             },
             {// 托管商标详情
                 path: 'trust/detail/:id',
-                data: {name: '商标托管详情'},
+                data: {name: '商标托管详情' , menuIndex: 2},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./trust/detail/detail.module').then(m => m.AdminTrustDetailPageModule)
             },
             {// 我的知产
                 path: 'box/list',
-                data: {name: '我的知产'},
+                data: {name: '我的知产', menuIndex: 4},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./box/list/list.module').then(m => m.AdminBoxListPageModule)
             },
             {// 添加知产
                 path: 'box/upload',
-                data: {name: '添加知产'},
+                data: {name: '添加知产', menuIndex: 4},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./box/upload/upload.module').then(m => m.AdminBoxUploadPageModule)
             },
             {// 工单列表
                 path: 'ticket/list',
-                data: {name: '我的工单'},
+                data: {name: '我的工单', menuIndex: null},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./ticket/list/list.module').then(m => m.AdminTicketListPageModule)
             },
             {// 工单详情
                 path: 'ticket/item/:id',
-                data: {name: '工单详情'},
+                data: {name: '工单详情', menuIndex: null},
                 canActivate: [CompanyGuard],
                 loadChildren: () =>
                     import('./ticket/item/item.module').then(m => m.AdminTicketItemPageModule)
--- src/app/pages/admin/admin.page.ts
+++ src/app/pages/admin/admin.page.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 import {LoadingController} from '@ionic/angular';
 import {DialogService} from '../../@core/modules/dialog';
 import {AuthService} from '../auth/auth.service';
 import {CompanyService} from './company/company.service';
 import {AccountService} from './account/account.service';
 import {PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
+import {filter, map, mergeMap} from 'rxjs/operators';
+import {NavigationEnd} from '@angular/router';
 
 @Component({
     selector: 'app-admin',
@@ -17,18 +20,39 @@ export class AdminPage {
     balance = 0;
     show = true;
     config: PerfectScrollbarConfigInterface = {};
+    menuIndex: number;
 
-    constructor(private loadingController: LoadingController,
+    constructor(private activatedRoute: ActivatedRoute,
+                private router: Router,
+                private loadingController: LoadingController,
                 private dialogSvc: DialogService,
                 private authSvc: AuthService,
                 private accountSvc: AccountService,
                 private companySvc: CompanyService) {
+        router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            map(() => this.activatedRoute),
+            map(route => {
+                while (route.firstChild) {
+                    route = route.firstChild;
+                }
+                return route;
+            }),
+            mergeMap(route => {
+                return route.data;
+            })
+        ).subscribe(data => {
+            this.menuIndex = data.menuIndex ? data.menuIndex : null;
+        });
         companySvc.company.subscribe(res => {
             this.company = res;
         });
         accountSvc.balance(this.company.id).subscribe(res => {
             this.balance = res;
         });
+    }
+
+    setActive(index) {
     }
 
     async presentLoading() {
