fd065d2fb9dd3138f6eeaba6a75b5f4e26fee4f0
wzh66 <wzh514500>
2020-02-24 16:04:44 +0800

    update

--- src/app/pages/admin/user/user.component.ts
+++ src/app/pages/admin/user/user.component.ts
@@ -34,7 +34,7 @@ export class AdminUserPage implements OnInit {
         payedDateBegin: '',
         payedDateEnd: ''
     };
-    displayedColumns: string[] = ['date', 'cost', 'payY', 'payZ', 'rechargeY', 'rechargeZ', 'refund'];
+    displayedColumns: string[] = ['date', 'cost', 'refund', 'payY', 'payZ', 'rechargeY', 'rechargeZ'];
     dataSource;
     selection = new SelectionModel<any>(true, []);
     date;
